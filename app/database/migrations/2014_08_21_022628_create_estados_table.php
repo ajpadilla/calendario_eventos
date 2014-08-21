339ba9ddc121651562e629370fa0db16abfb77b2
ajpadilla <ajpadilla88@gmail.com>
2014-08-20 22:05:33 -0430

    Agregando campo nombre a la tabla estados

---
+++ app/database/migrations/2014_08_21_022628_create_estados_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateEstadosTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('estados', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('nombre',45);
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('estados');
+	}
+
+}
