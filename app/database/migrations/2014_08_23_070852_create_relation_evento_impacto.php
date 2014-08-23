283ab4449262970a110e67efdb078e36d50d753e
night zpy <nightzpy@nightzpy.nightzpy.com>
2014-08-23 03:31:13 -0430

    Agregando migracion, para la relacion entre los eventos he impactos

---
+++ app/database/migrations/2014_08_23_070852_create_relation_evento_impacto.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateRelationEventoImpacto extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('eventos', function(Blueprint $table)
+		{
+            $table->integer('impacto_id')->unsigned()->before('nombre');
+            $table->foreign('impacto_id')
+                  ->references('id')
+                  ->on('impactos')
+                  ->onDelete('no action')
+                  ->onUpdate('cascade');    
+	    });
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        $table->dropForeign('eventos_impacto_id_foreign');
+        $table->dropColumn('impacto_id');
+
+	}
+
+}
