cde00c5087061eaf4205b034f71b01f8a484e1ee
night zpy <nightzpy@nightzpy.nightzpy.com>
2014-08-23 03:28:00 -0430

    Agregando migracion para relacionar a los eventos con las personas

---
+++ app/database/migrations/2014_08_23_074016_create_beneficiarios.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateBeneficiarios extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('beneficiarios', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->enum('tipo',array('espectadores','directos'));
+            $table->integer('evento_id')->unsigned();
+            $table->integer('persona_id')->unsigned();
+			$table->timestamps();
+            $table->softdeletes();
+            $table->unique(array('evento_id', 'persona_id'), 'beneficiarios_unique');
+		});
+        
+        Schema::table('beneficiarios', function(Blueprint $table)
+        {
+            $table->foreign('evento_id')
+                    ->references('id')
+                    ->on('eventos')
+                    ->onDelete('no action')
+                    ->onUpdate('cascade');
+
+            $table->foreign('persona_id')
+                    ->references('id')
+                    ->on('personas')
+                    ->onDelete('no action')
+                    ->onUpdate('cascade');  
+        });
+
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('beneficiarios');
+	}
+
+}
