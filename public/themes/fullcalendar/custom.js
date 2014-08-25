e267491e6f3dca3cce5fc95157b9bfd4a32f687f
ajpadilla <ajpadilla88@gmail.com>
2014-08-25 19:20:39 -0430

    Agregando metodos para seleccionar y actualizar el calendario

--- public/themes/fullcalendar/custom.js
+++ public/themes/fullcalendar/custom.js
@@ -1,33 +1,39 @@
 $(document).ready(function() {
-    	$('#calendar').fullCalendar({
-              header: {
-                  left: 'prev,next today',
-                  center: 'title',
-                  right: 'month,agendaWeek,agendaDay'
-              },
-              defaultDate: '2014-08-12',
-              selectable: true,
-              selectHelper: true,
-              select: function(start, end) {
-                  var title = prompt('Event Title:');
-                  var eventData;
-                  if (title) {
-                      eventData = {
-                          title: title,
-                          start: start,
-                          end: end
-                      };
-                      $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
-                  }
-                  $('#calendar').fullCalendar('unselect');
-              },
-              editable: true,
-              eventLimit: true, // allow "more" link when too many events
-			  events: [
-              {
-                      title: 'All Day Event',
-                      start: '2014-08-01'
-              }],
+	$('#calendar').fullCalendar({
+    	header: {
+        	left: 'prev,next today',
+       		center: 'title',
+      		right: 'month,agendaWeek,agendaDay'
+   		},
+   		defaultDate: '2014-08-12',
+      	selectable: true,
+       	selectHelper: true,
+      	select: function(start, end) {
+        	var title = prompt('Event Title:');
+            var eventData;
+		    if (title) {
+				eventData = {
+               		title: title,
+                	start: $.fullCalendar.moment(start).format(),
+           		};
+				console.log(eventData);
+               	$('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+            }
+			$('#calendar').fullCalendar('unselect');
+      	},
+        editable: true,
+        eventLimit: true, // allow "more" link when too many events
+		events: [
+        {
+        	title: 'All Day Event',
+            start: '2014-08-01'
+        }],
+		eventClick: function(event, element) {
 
+        	event.title =  prompt('Event Title:');
+
+        	$('#calendar').fullCalendar('updateEvent', event);
+
+   		 }	
 	});
 });
