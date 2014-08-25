5b1469668de512ae8b0d41183480d9d6e10dd929
ajpadilla <ajpadilla88@gmail.com>
2014-08-25 11:50:31 -0430

    Modificando el modelo

--- app/models/Estado.php
+++ app/models/Estado.php
@@ -4,4 +4,9 @@ class Estado extends Model {
 	protected $table = 'estados';
 	protected $fillable = array();
 	public static $rules = array();
+	
+	public function albums()
+	{
+		return $this->hasMany('Album');
+	}
 }
