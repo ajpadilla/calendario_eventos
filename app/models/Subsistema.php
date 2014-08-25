2e4a25382eccb2ac980b7f929c90dd8c6440431e
ajpadilla <ajpadilla88@gmail.com>
2014-08-25 12:26:59 -0430

    Agregando metodo eventos

--- app/models/Subsistema.php
+++ app/models/Subsistema.php
@@ -4,5 +4,9 @@ class Subsistema extends Model {
 	protected $table = 'subsistemas';
 	protected $fillable = array();
 	public static $rules = array();
+	
+	public function Eventos(){
+	 	return $this->hasMany('Evento');
+	}
 
 }
