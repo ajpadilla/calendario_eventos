c33beeefe2591c8314d60a9666254c5c6d4a8777
ajpadilla <ajpadilla88@gmail.com>
2014-08-25 12:33:05 -0430

    AGregando metodos para relacionar el modelo con los modelos: impacto, municipio,articulacio, subsistema

--- app/models/Evento.php
+++ app/models/Evento.php
@@ -5,6 +5,19 @@ class Evento extends Model {
 	protected $fillable = array();
 	public static $rules = array();
 
+	public function subsistema(){
+		 return $this->belongsTo('Subsistema');
+	}
 	
+	public function impacto(){
+		 return $this->belongsTo('Impacto');
+	}
+	
+	public function articulacion(){
+		return $this->belongsTo('Articulacion');
+	}
 
+	public function municipio(){
+		return $this->belongsTo('Municipio');
+	}
 }
