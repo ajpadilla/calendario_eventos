0168623ce9169c9e75e76b962979dc9258597eb8
ajpadilla <ajpadilla88@gmail.com>
2014-08-26 01:52:09 -0430

    Modificando archivo

--- public/themes/fullcalendar/custom.js
+++ public/themes/fullcalendar/custom.js
@@ -1,61 +1,51 @@
 $(document).ready(function() {
-	$('#calendar').fullCalendar({
-    	header: {
-        	left: 'prev,next today',
-       		center: 'titulo',
-      		right: 'month,agendaWeek,agendaDay'
-   		},
-   		defaultDate: '2014-08-12',
-      	selectable: true,
-       	selectHelper: true,
-		events: [
-        {
-        	titulo: 'All Day Event',
-            start: '2014-08-01'
-        }],
-      	select: function(start, end) {
-        	var titulo = prompt('Event Title:');
-			/*var descripcion = prompt('Event Descripcion:');
-			var hora = prompt('Event Hora:');
-			var direccion = prompt('Event Direccion:');
-			var observacion = prompt('Event Observacion:');
-			var articulacion = prompt('Event Articulacion:');
-			var impacto = prompt('Event Impacto:');
-			var subsistema = prompt('Event subsistema:');
-			var municipio = prompt('Event Municipio:');*/
-
-            var eventData;
-		    if (titulo) {
-				eventData = {
-               		titulo: titulo,
-					/*descripcion: descripcion,
-             	   	fecha: $.fullCalendar.moment(start).format()+' '+hora,
-					direccion: direccion,
-					observacion: observacion,
-					articulacion: articulacion,
-					impacto: impacto,
-					subsistema:subsistema,
-					municipio:municipio*/
-           		};
-				$.ajax({
-                  	type: "POST",
-                	url:'eventos/' + JSON.stringify(eventData),
-                  	success: function(response) {
-						console.log(response);
-                  	},
-					error : function(jqXHR, status, error) {
-						console.log('Disculpe, existió un problema');
-					},
-              	});
-               	$('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
-            }
-			$('#calendar').fullCalendar('unselect');
-      	},
-        editable: true,
-        eventLimit: true, // allow "more" link when too many events
-		eventClick: function(event, element) {
-        	event.titulo =  prompt('Event Title:');
-        	$('#calendar').fullCalendar('updateEvent', event);
-   		 }	
+		
+		$('#calendar').fullCalendar({
+			header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,agendaWeek,agendaDay'
+			},
+			defaultDate: '2014-08-12',
+			selectable: true,
+			selectHelper: true,
+			select: function(start, end) {
+				var title = prompt('Event Title:');
+				var eventData;
+				if (title) {
+					eventData = {
+						title: title,
+						start: start,
+						end: end
+					};
+					$('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+					
+					$.ajax({
+						type: "POST",
+						url:'eventos/' + JSON.stringify(eventData),
+						success: function(response) {
+							console.log(response);
+						},
+						error : function(jqXHR, status, error) {
+							console.log('Disculpe, existió un problema');
+						},
+					});
+				}
+				$('#calendar').fullCalendar('unselect');
+			},
+			editable: true,
+			eventLimit: true, // allow "more" link when too many events
+			events: [
+				{
+					title: 'All Day Event',
+					start: '2014-08-01'
+				},
+			],
+			eventClick: function(event, element) {
+				event.title = prompt('Nuevo Title:');
+				$('#calendar').fullCalendar('updateEvent', event);
+			}
+		});
+		
 	});
-});
+
