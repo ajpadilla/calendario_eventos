1e66e06d0f54ed22e447e0e84ccb3f4b6c8c140a
ajpadilla <ajpadilla88@gmail.com>
2014-08-27 18:50:26 -0430

    Agregando division de secciones para los formularios del proyecto

---
+++ app/views/themes/fullcalendar/layouts/form_content.blade.php
@@ -0,0 +1,25 @@
+@extends('themes.fullcalendar.layouts.default')
+
+@section('body')
+<div class="container">
+	<div class="row">
+    	<div id="content" class="col-lg-12">
+        	<!-- PAGE HEADER-->
+            <div class="row">
+            	<div class="col-sm-12">
+               		<div class="page-header">
+                    	@yield('page_header')
+                    </div>
+                </div>
+           	</div>
+        	<!-- /PAGE HEADER -->
+           	<!-- FORMS -->
+            <div class="row">
+           		<div class="col-md-12">
+              		@yield('form')
+           		</div>
+        	</div>
+     	</div>
+	</div>
+</div>
+@stop
