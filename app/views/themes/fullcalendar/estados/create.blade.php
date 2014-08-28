5464965c81fa4004c78dae1fdb36cd60ea258933
ajpadilla <ajpadilla88@gmail.com>
2014-08-27 21:47:43 -0430

    Incluyendo formulario para agregar los estados

--- app/views/themes/fullcalendar/estados/create.blade.php
+++ app/views/themes/fullcalendar/estados/create.blade.php
@@ -21,7 +21,7 @@
 @section('form')
 	<div class="box border blue">
           <div class="box-title">
-              <h4><i class="fa fa-reorder"></i>Calendario de eventos</h4>
+              <h4><i class="fa fa-reorder"></i>Agregar estado</h4>
               <div class="tools hidden-xs">
                   <a href="#box-config" data-toggle="modal" class="config">
                       <i class="fa fa-cog"></i>
@@ -38,7 +38,7 @@
               </div>
           </div>
           <div class="box-body form">
-               <div id='calendar'></div>
+				@include('themes.fullcalendar.estados.form')
           </div>
 	</div>
 @stop
