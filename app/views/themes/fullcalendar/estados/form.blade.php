fa08949f02a1a7c2d812d3f1cffdef4ed48def6b
ajpadilla <ajpadilla88@gmail.com>
2014-08-27 21:48:14 -0430

    Agregado etiqueta form al formulario

--- app/views/themes/fullcalendar/estados/form.blade.php
+++ app/views/themes/fullcalendar/estados/form.blade.php
@@ -1,6 +1,8 @@
-<div class="form-group">
-	{{ Form::label('nombre', 'Nombre: ', array('class' => 'col-md-3 control-label')) }}
-    <div class="col-md-8">
-   		{{ Form::text('nombre', Input::old('nombre'), array('class' => 'form-control','id'=>nombree')) }}
-    </div>
-</div>
+<form class="form-horizontal">
+	<div class="form-group">
+		{{ Form::label('nombre', 'Nombre: ', array('class' => 'col-md-3 control-label')) }}
+    	<div class="col-md-8">
+   			{{ Form::text('nombre', Input::old('nombre'), array('class' => 'form-control','id'=>'nombre')) }}
+    	</div>
+	</div>
+<form>
