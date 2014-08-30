b1850a779f88afa667e4ba14d850bd34a5d21b50
ajpadilla <ajpadilla88@gmail.com>
2014-08-30 18:10:10 -0430

    Indentando espacios entre lineas

--- public/themes/fullcalendar/scripts.js
+++ public/themes/fullcalendar/scripts.js
@@ -23,42 +23,41 @@ $(document).ready(function() {
 			form.submit();
 		}
 	});
+	$.ajax({
+		type: 'GET',
+		url: 'cargarEstados',
+		dataType:'json',
+		success: function(response){
+			//console.log(response.estados);
+			if(response.success == true) {
+				$('#estados').html('');
+				$('#estados').append('<option value=\"\">-- Estado --</option>');
+					$.each(response.estados,function (k,v){
+					$('#estados').append('<option value=\"'+k+'\">'+v+'</option>');
+				});
+				}else{
+					$('#estados').html('');
+					$('#estados').append('<option value=\"\">-- Estado --</option>');
+				}
+			},
+			error:function(jqXHR, status, error) {
+            	console.log('Disculpe, existió un problema');
+            },
+		});
 	
-			$.ajax({
-				type: 'GET',
-				url: 'cargarEstados',
-				dataType:'json',
-				success: function(response){
-					//console.log(response.estados);
-					if(response.success == true) {
-						$('#estados').html('');
-						$('#estados').append('<option value=\"\">-- Estado --</option>');
-						$.each(response.estados,function (k,v){
-						$('#estados').append('<option value=\"'+k+'\">'+v+'</option>');
-					});
-					}else{
-						$('#estados').html('');
-						$('#estados').append('<option value=\"\">-- Estado --</option>');
-					}
-				},
-				error:function(jqXHR, status, error) {
-                	console.log('Disculpe, existió un problema');
-               	},
-			});
-	
-		$('#estados').click(function(){
-			console.log('estado:'+$('#estados').val());
-			$.ajax({
-				type: "GET",
-				url:'cargarMunicipios/'+$('#estados').val(),
-				dataType:'json',
-				success: function(response) {
-					//console.log('Municipios'+JSON.stringify(response));
-					if(response.success == true) {
-                          $('#municipios').html('');
-                          $('#municipios').append('<option value=\"\">-- Municipio --</option>');
-                          $.each(response.municipios,function (k,v){
-                          	$('#municipios').append('<option value=\"'+k+'\">'+v+'</option>');
+	$('#estados').click(function(){
+		//console.log('estado:'+$('#estados').val());
+		$.ajax({
+			type: "GET",
+			url:'cargarMunicipios/'+$('#estados').val(),
+			dataType:'json',
+			success: function(response) {
+				//console.log('Municipios'+JSON.stringify(response));
+				if(response.success == true) {
+                	$('#municipios').html('');
+                    $('#municipios').append('<option value=\"\">-- Municipio --</option>');
+                   		$.each(response.municipios,function (k,v){
+                        	$('#municipios').append('<option value=\"'+k+'\">'+v+'</option>');
                       	}); 
                       }else{
                           $('#estados').html('');
@@ -72,51 +71,51 @@ $(document).ready(function() {
 
 		});
 		
-		$.ajax({
-			type: "GET",
-			url:'retornarArticulaciones',
-			dataType:'json',
-			success: function(response) {
-				//console.log('Arti:'+response);
-				if (response.success == true) {
-					$('#articulaciones').html('');
-					$('#articulaciones').append('<option value=\"\">-- Articulación --</option>');
-					$.each(response.articulaciones,function (k,v){
-						$('#articulaciones').append('<option value=\"'+k+'\">'+v+'</option>');
-					});
-					}else{
-						$('#articulaciones').html('');
-						$('#articulaciones').append('<option value=\"\">-- Articulación --</option>');
-					}
-			},
-			error : function(jqXHR, status, error) {
-				console.log('Disculpe, existió un problema');
-			},
-		});
+	$.ajax({
+		type: "GET",
+		url:'retornarArticulaciones',
+		dataType:'json',
+		success: function(response) {
+			//console.log('Arti:'+response);
+		if (response.success == true) {
+			$('#articulaciones').html('');
+			$('#articulaciones').append('<option value=\"\">-- Articulación --</option>');
+			$.each(response.articulaciones,function (k,v){
+				$('#articulaciones').append('<option value=\"'+k+'\">'+v+'</option>');
+			});
+			}else{
+				$('#articulaciones').html('');
+				$('#articulaciones').append('<option value=\"\">-- Articulación --</option>');
+			}
+		},
+		error : function(jqXHR, status, error) {
+			console.log('Disculpe, existió un problema');
+		},
+	});
 		
-		$.ajax({
-              type: "GET",
-              url:'retornarImpactos',
-              dataType:'json',
-              success: function(response) {
-                  //console.log('impacto:'+response);
-                  if (response.success == true) {
-                      $('#impactos').html('');
-                      $('#impactos').append('<option value=\"\">-- Impacto --</option>');
-                      $.each(response.impactos,function (k,v){
-                          $('#impactos').append('<option value=\"'+k+'\">'+v+'</option>');
-                      });
-                      }else{
-                          $('#impactos').html('');
-                          $('#impactos').append('<option value=\"\">-- Impacto --</option>');
-                      }
-              },
-              error : function(jqXHR, status, error) {
-                  console.log('Disculpe, existió un problema');
-              },
-          });
+	$.ajax({
+   		type: "GET",
+        url:'retornarImpactos',
+        dataType:'json',
+        success: function(response) {
+        	//console.log('impacto:'+response);
+        	if (response.success == true) {
+            	$('#impactos').html('');
+             	$('#impactos').append('<option value=\"\">-- Impacto --</option>');
+             	$.each(response.impactos,function (k,v){
+                	$('#impactos').append('<option value=\"'+k+'\">'+v+'</option>');
+                });
+               }else{
+               		$('#impactos').html('');
+                    $('#impactos').append('<option value=\"\">-- Impacto --</option>');
+               }
+          	},
+            error : function(jqXHR, status, error) {
+            	console.log('Disculpe, existió un problema');
+            },
+     });
 
-		 $.ajax({
+	$.ajax({
 			type: 'GET',
 			url: 'retornarSubsistemas',
 			dataType:'json',
