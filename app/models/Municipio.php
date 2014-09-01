25069072b4bd65fe4417a8a82f9492c4beb14428
night zpy <nightzpy@nightzpy.nightzpy.com>
2014-08-31 21:21:41 -0430

    Actualizando modelo

--- app/models/Municipio.php
+++ app/models/Municipio.php
@@ -4,7 +4,11 @@ class Municipio extends \Eloquent {
 	protected $table = 'municipios';
 	protected $fillable = array();
 	public static $rules = array();
-	
+    
+    public function eventos(){
+        return $this->hasMany('Evento');
+    }
+    	
 	public function estado()
 	{
 		return $this->belongsTo('Estado');
