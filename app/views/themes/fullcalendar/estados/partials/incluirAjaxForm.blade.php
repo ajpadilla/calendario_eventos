aa23eb2669e0cf51703567c6417958bf9fcb9505
ajpadilla <ajpadilla88@gmail.com>
2014-09-02 23:53:52 -0430

    Enviando datos del estado al servidor, usando la funcion $.ajax

--- app/views/themes/fullcalendar/estados/partials/incluirAjaxForm.blade.php
+++ app/views/themes/fullcalendar/estados/partials/incluirAjaxForm.blade.php
@@ -14,8 +14,45 @@
 			});
 			$('#enviar').click(function() {
 				console.log('Hola');
-    			alert($('#formEstado').valid());
+				if($('#formEstado').valid() == 1){
+					$.ajax({
+							type:'POST',
+							url:'" . URL::to('/guardarEstado/') ."',
+							data:$('#formEstado').serialize(),
+							dataType:'json',
+							success : function(response) {
+								console.log(response);
+								$.fancybox({
+									'content': '<h1>Estado</h1>' + response.nombre +'<h1>Agregado</h1>',
+									'autoScale' : true,
+									'transitionIn' : 'none',
+									'transitionOut' : 'none',
+									'scrolling' : 'no',
+									'type' : 'inline',
+									'showCloseButton' : false,
+									'hideOnOverlayClick' : false,
+									'hideOnContentClick' : false
+								}); 
+								$('#formEstado').clearForm();
+							},
+							error : function(jqXHR, status, error) {
+								alert('Disculpe, existió un problema');
+								$.fancybox({
+									'content': '<h1>Error al enviar los datos al servidor</h1>',
+									'autoScale' : true,
+									'transitionIn' : 'none',
+									'transitionOut' : 'none',
+									'scrolling' : 'no',
+									'type' : 'inline',
+									'showCloseButton' : false,
+									'hideOnOverlayClick' : false,
+									'hideOnContentClick' : false
+								}); 
+								$('#formEstado').clearForm();
+							},
+						});
+					}
+				});
 			});
-		});
 	"
 }}
