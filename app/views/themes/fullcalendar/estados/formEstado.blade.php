a38d938bbc7a12b03a1f7934887b78d12b682205
ajpadilla <ajpadilla88@gmail.com>
2014-09-04 16:14:15 -0430

    Modificando el nombre del text

--- app/views/themes/fullcalendar/estados/formEstado.blade.php
+++ app/views/themes/fullcalendar/estados/formEstado.blade.php
@@ -1,7 +1,7 @@
 	<div class="form-group">
 		{{ Form::label('nombre', 'Nombre: ', array('class' => 'col-md-3 control-label')) }}
 		<div class="col-md-8">
-		{{ Form::text('nombre', Input::old('nombre'), array('class' => 'form-control','id'=>'nombre')) }}
+		{{ Form::text('nombre', Input::old('nombre'), array('class' => 'form-control','id'=>'nombreEstado')) }}
 		</div>
 	</div>
 	<div class="row">
