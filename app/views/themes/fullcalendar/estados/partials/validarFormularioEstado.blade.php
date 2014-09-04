5a0a76801b5ca8260bfdab31a8ce11409c381af1
ajpadilla <ajpadilla88@gmail.com>
2014-09-04 16:47:59 -0430

    Agregando validacion, para agregar el estado

--- app/views/themes/fullcalendar/estados/partials/validarFormularioEstado.blade.php
+++ app/views/themes/fullcalendar/estados/partials/validarFormularioEstado.blade.php
@@ -15,43 +15,53 @@
 		
 			$('#registrarEstado').click(function(){
 				console.log($('#formWizardEstado').valid());
-				$.ajax({
-					type:'POST',
-					url:'" . URL::to('/guardarEstado/') ."',
-					data:{ nombre: $('#nombreEstado').val()},
-					dataType:'json',
-					success : function(response) {
-						console.log(response);
-						$.fancybox({
-							'content': '<h1>Estado Agregado</h1>',
-							'autoScale' : true,
-							'transitionIn' : 'none',
-							'transitionOut' : 'none',
-							'scrolling' : 'no',
-							'type' : 'inline',
-							'showCloseButton' : false,
-							'hideOnOverlayClick' : false,
-							'hideOnContentClick' : false
+				if($('#formWizardEstado').valid() == true){
+						$.ajax({
+							type:'POST',
+							url:'" . URL::to('/guardarEstado/') ."',
+							data:{ nombre: $('#nombreEstado').val()},
+							dataType:'json',
+							success : function(response) {
+								console.log(response);
+								$.fancybox({
+									'content': '<h1>Estado Agregado</h1>',
+									'autoScale' : true,
+									'transitionIn' : 'none',
+									'transitionOut' : 'none',
+									'scrolling' : 'no',
+									'type' : 'inline',
+									'showCloseButton' : false,
+									'hideOnOverlayClick' : false,
+									'hideOnContentClick' : false
+								});
+								$('#formWizardEstado').clearForm();
+							},
+							error : function(jqXHR, status, error) {
+								alert('Disculpe, existió un problema');
+								$.fancybox({
+									'content': '<h1>Error al agregar el estado</h1>',
+									'autoScale' : true,
+									'transitionIn' : 'none',
+									'transitionOut' : 'none',
+									'scrolling' : 'no',
+									'type' : 'inline',
+									'showCloseButton' : false,
+									'hideOnOverlayClick' : false,
+									'hideOnContentClick' : false
+								});
+								$('#formWizardEstado').clearForm();
+							},
 						});
-						$('#formWizardEstado').clearForm();
-					},
-					error : function(jqXHR, status, error) {
-						alert('Disculpe, existió un problema');
-						$.fancybox({
-							'content': '<h1>Error al agregar el estado</h1>',
-							'autoScale' : true,
-							'transitionIn' : 'none',
-							'transitionOut' : 'none',
-							'scrolling' : 'no',
-							'type' : 'inline',
-							'showCloseButton' : false,
-							'hideOnOverlayClick' : false,
-							'hideOnContentClick' : false
-						});
-						$('#formWizardEstado').clearForm();
-					},
-				});
+				}
 			});	
+
+			$('.borrarEstado').click(function(){
+				console.log('Borrar:');
+			});
+			
+			$('.editarEstado').click(function(){
+				console.log('Editar');
+			});
 		
 		});
 	"
