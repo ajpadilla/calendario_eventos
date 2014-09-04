1b9ccd2fb379267626f6acc3265d5bce86179812
ajpadilla <ajpadilla88@gmail.com>
2014-09-03 22:06:35 -0430

    Agregando blade para editar los estados

---
+++ app/views/themes/fullcalendar/estados/edit.blade.php
@@ -0,0 +1,54 @@
+@extends('themes.fullcalendar.layouts.form_content')
+
+@section('')
+@stop();
+
+@section('page_header')
+	<ul class="breadcrumb">
+		<li>
+			<i class="fa fa-home"></i>
+			<a href="#">Home</a>
+		</li>
+		<li>
+			<a href="#">Form Elements</a>
+			</li>
+		<li>Forms</li>
+	</ul>
+		<!-- /BREADCRUMBS -->
+	<div class="clearfix">
+		<h3 class="content-title pull-left">Forms</h3>
+	</div>
+	<div class="description">Calendario de eventos</div>
+@stop
+@section('form')
+	<div class="box border blue">
+		<div class="box-title">
+			<h4><i class="fa fa-reorder"></i>Agregar estado</h4>
+			<div class="tools hidden-xs">
+				<a href="#box-config" data-toggle="modal" class="config">
+					<i class="fa fa-cog"></i>
+				</a>
+				<a href="javascript:;" class="reload">
+					<i class="fa fa-refresh"></i>
+				</a>
+				<a href="javascript:;" class="collapse">
+					<i class="fa fa-chevron-up"></i>
+				</a>
+				<a href="javascript:;" class="remove">
+					<i class="fa fa-times"></i>
+				</a>
+			</div>
+		</div>
+		<div class="box-body form">
+			<h1>Editar estado {{ $estado->nombre }}</h1>
+			{{ Form::open(array('url' => array('actualizarEstado', $estado->id),'class'=>'form-horizontal','id'=>'formEstadoUpdate','novalidate'=>'novalidate','files'=>true,'method' => 'PUT')) }}
+				<div class="form-group">
+					{{ Form::label('nombre', 'Nombre: ', array('class' => 'col-md-3 control-label')) }}
+					<div class="col-md-8">
+						{{ Form::text('nombre', $estado->nombre, array('class' => 'form-control','id'=>'nombre')) }}
+					</div>
+				</div>
+			{{ Form::close() }}
+		</div>
+	</div>
+@stop
