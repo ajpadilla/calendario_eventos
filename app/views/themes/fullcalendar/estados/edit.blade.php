ed84ef5f0c96c9b95efb3ef6c0001562781c5264
ajpadilla <ajpadilla88@gmail.com>
2014-09-04 14:59:51 -0430

    Editando estados

--- app/views/themes/fullcalendar/estados/edit.blade.php
+++ app/views/themes/fullcalendar/estados/edit.blade.php
@@ -1,6 +1,7 @@
 @extends('themes.fullcalendar.layouts.form_content')
 
-@section('')
+@section('script')
+	@include('themes.fullcalendar.estados.partials.validarEstadoUpdate')
 @stop();
 
 @section('page_header')
@@ -41,11 +42,23 @@
 		</div>
 		<div class="box-body form">
 			<h1>Editar estado {{ $estado->nombre }}</h1>
-			{{ Form::open(array('url' => array('actualizarEstado', $estado->id),'class'=>'form-horizontal','id'=>'formEstadoUpdate','novalidate'=>'novalidate','files'=>true,'method' => 'PUT')) }}
+			{{ Form::open(array('action' => array('EstadoController@store'),'class'=>'form-horizontal','id'=>'formEstadoUpdate','novalidate'=>'novalidate','files'=>true)) }}
 				<div class="form-group">
 					{{ Form::label('nombre', 'Nombre: ', array('class' => 'col-md-3 control-label')) }}
 					<div class="col-md-8">
 						{{ Form::text('nombre', $estado->nombre, array('class' => 'form-control','id'=>'nombre')) }}
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-md-8">
+						{{ Form::text('id', $estado->id, array('class' => 'form-control','style'=>'display: none;','id'=>'id_estado')) }}
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="col-md-offset-3 col-md-9">
+							<a id="enviar" href="javascript:;" class="btn btn-primary nextBtn">Guardar<i class="fa fa-arrow-circle-right"></i></a>
+						</div>
 					</div>
 				</div>
 			{{ Form::close() }}
