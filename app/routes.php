12ae836085f365c71f8583f81de4b6ae589288b8
ajpadilla <ajpadilla88@gmail.com>
2014-09-04 14:58:28 -0430

    Organizando rutas del controlador municipios

--- app/routes.php
+++ app/routes.php
@@ -19,9 +19,12 @@ Route::get('/', function()
 Route::get('retornarArticulaciones','ArticulacionController@retornarArticulaciones');
 Route::get('retornarImpactos','ImpactoController@retornarImpactos');
 Route::get('retornarSubsistemas','SubsistemaController@retornarSubsistemas');
+
+
 Route::get('cargarMunicipios/{id_estado}','MunicipioController@cargarMunicipios');
-//Route::resource('estado','EstadoController');
 Route::resource('municipios','MunicipioController');
+
+//Route::resource('estado','EstadoController');
 //Route::post('eventos/{datos}','EventController@store');
 
 
@@ -30,6 +33,9 @@ Route::get('crearEstado','EstadoController@create');
 Route::post('guardarEstado','EstadoController@store');
 Route::post('verificarExistenciaNombreEstado','EstadoController@verificarExistenciaNombreEstado');
 Route::get('mostrarEstados','EstadoController@index');
+Route::get('editarEstado/{id}','EstadoController@edit');
+Route::post('actualizarEstado/{id}','EstadoController@update');
+Route::get('borrarEstado/{id}','EstadoController@destroy');
 
 Route::post('updateStartEvent/{event_data}','EventController@updateStartEvent');
 Route::get('cargar_eventos','EventController@allEvents');
