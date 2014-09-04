d6cadc5a23b0ecbae805cf79457283b0318466ef
ajpadilla <ajpadilla88@gmail.com>
2014-09-04 15:48:58 -0430

    Incluyendo plugin validate

--- app/views/themes/fullcalendar/estados/partials/validarFormularioEstado.blade.php
+++ app/views/themes/fullcalendar/estados/partials/validarFormularioEstado.blade.php
@@ -6,7 +6,17 @@
 			$.validator.setDefaults({
  				debug: true,
 			});
-
+			
+			$('#formWizardEstado').validate({
+				rules:{
+					nombre:{required:!0},
+				}
+			});
+		
+			$('#registrarEstado').click(function(){
+				console.log($('#formWizardEstado').valid());
+			});	
+		
 		});
 	"
 }}
