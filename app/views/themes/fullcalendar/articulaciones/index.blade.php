835676c01a2029809da75132d4c0d4a7a292e241
ajpadilla <ajpadilla88@gmail.com>
2014-09-04 21:03:58 -0430

    Agregando blades para las articulaciones

---
+++ app/views/themes/fullcalendar/articulaciones/index.blade.php
@@ -0,0 +1,20 @@
+<table class="table table-striped table-bordered">
+	<thead>
+		<tr>
+			<td>Id</td>
+			<td>Nombre</td>
+			<td>Editar</td>
+			<td>Borrar</td>
+		</tr>
+	</thead>
+		<tbody>
+			@foreach($estados as $estado)
+			<tr>
+				<td>{{ $estado->id }}</td>
+				<td>{{ $estado->nombre }}</td>
+				<td><button  type="button" class="editarEstado btn btn-primary">Editar</button></td>
+				<td><button  type="button" class="borrarEstado btn btn-danger">Borrar</button></td>
+			</tr>
+			@endforeach
+		</tbody>
+</table>
