41cc3c8c36f2ce79f6fa0c34cbb16b5d76db878c
ajpadilla <ajpadilla88@gmail.com>
2014-09-05 16:31:20 -0430

    Agregando columna estado

--- app/views/themes/fullcalendar/municipios/index.blade.php
+++ app/views/themes/fullcalendar/municipios/index.blade.php
@@ -3,6 +3,7 @@
            <tr>
                <td>Id</td>
                <td>Nombre</td>
+               <td>Estado</td>
                <td>Editar</td>
                <td>Borrar</td>
            </tr>
@@ -12,6 +13,7 @@
               <tr>
                   <td>{{ $municipio->id }}</td>
                   <td>{{ $municipio->nombre }}</td>
+                  <td>{{ $municipio->estado->nombre}}</td>
 				  <td><a  href="{{URL::to('editarMunicipio/'.$municipio->id)}}" class="btn btn-primary">Editar<i class="fa fa-arrow-circle-right"></i></a></td>
                   <td><a  href="" class="btn btn-danger">Borrar<i class="fa fa-arrow-circle-right"></i></a></td>
               </tr>
