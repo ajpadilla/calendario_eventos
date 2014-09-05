83894dd19aeb81e35c934480bed702207c683f78
ajpadilla <ajpadilla88@gmail.com>
2014-09-04 21:42:40 -0430

    Actualizando archivo

--- app/views/themes/fullcalendar/articulaciones/form.blade.php
+++ app/views/themes/fullcalendar/articulaciones/form.blade.php
@@ -1,7 +1,7 @@
 	<div class="form-group">
 		{{ Form::label('nombre', 'Nombre: ', array('class' => 'col-md-3 control-label')) }}
 		<div class="col-md-8">
-		{{ Form::text('nombre', Input::old('nombre'), array('class' => 'form-control','id'=>'nombreEstado')) }}
+		{{ Form::text('nombre', Input::old('nombre'), array('class' => 'form-control','id'=>'nombre')) }}
 		</div>
 	</div>
 	<div class="row">
