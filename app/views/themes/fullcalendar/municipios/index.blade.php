4e14904cebeec4bcfdf5f29c7b2b05f4f01a237c
ajpadilla <ajpadilla88@gmail.com>
2014-09-04 21:31:36 -0430

    Actuializando archivo

--- app/views/themes/fullcalendar/municipios/index.blade.php
+++ app/views/themes/fullcalendar/municipios/index.blade.php
@@ -1,5 +1,5 @@
  <table class="table table-striped table-bordered">
-       <thead>
+  2     <thead>
            <tr>
                <td>Id</td>
                <td>Nombre</td>
@@ -8,10 +8,10 @@
            </tr>
        </thead>
           <tbody>
-              @foreach($estados as $estado)
+              @foreach($municipios as $municipio)
               <tr>
-                  <td>{{ $estado->id }}</td>
-                  <td>{{ $estado->nombre }}</td>
+                  <td>{{ $municipio->id }}</td>
+                  <td>{{ $municipio->nombre }}</td>
                   <td><button  type="button" class="editarEstado btn btn-primary">Editar</button></td>
                   <td><button  type="button" class="borrarEstado btn btn-danger">Borrar</button></td>
               </tr>
