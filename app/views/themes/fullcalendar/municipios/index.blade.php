1d5b0484c4959be77134713642a567f52642ac38
ajpadilla <ajpadilla88@gmail.com>
2014-09-04 21:11:10 -0430

    Mostrando articulaciones

--- app/views/themes/fullcalendar/municipios/index.blade.php
+++ app/views/themes/fullcalendar/municipios/index.blade.php
@@ -8,10 +8,10 @@
            </tr>
        </thead>
           <tbody>
-              @foreach($municipios as $municipio)
+              @foreach($estados as $estado)
               <tr>
-                  <td>{{ $municipio->id }}</td>
-                  <td>{{ $municipio->nombre }}</td>
+                  <td>{{ $estado->id }}</td>
+                  <td>{{ $estado->nombre }}</td>
                   <td><button  type="button" class="editarEstado btn btn-primary">Editar</button></td>
                   <td><button  type="button" class="borrarEstado btn btn-danger">Borrar</button></td>
               </tr>
