4931d71abf94ff22328055302e8d078307c51b11
ajpadilla <ajpadilla88@gmail.com>
2014-09-04 21:06:06 -0430

    Modificando id del boton enviar

--- app/views/themes/fullcalendar/articulaciones/form.blade.php
+++ app/views/themes/fullcalendar/articulaciones/form.blade.php
@@ -7,7 +7,7 @@
 	<div class="row">
 		<div class="col-md-12">
 			<div class="col-md-offset-3 col-md-9">
-				<a id="registrarEstado" href="javascript:;" class="btn btn-primary nextBtn">Registrar<i class="fa fa-arrow-circle-right"></i></a>
+				<a id="registrar" href="javascript:;" class="btn btn-primary nextBtn">Registrar<i class="fa fa-arrow-circle-right"></i></a>
 			</div>
 		</div>
 	</div>
