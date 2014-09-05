781c95d92cd428c4ff5c08465996b2dc7cd71355
ajpadilla <ajpadilla88@gmail.com>
2014-09-05 15:47:38 -0430

    Mostrando estados al hacer click en el select

--- app/views/themes/fullcalendar/municipios/partials/actualizarMunicipio.blade.php
+++ app/views/themes/fullcalendar/municipios/partials/actualizarMunicipio.blade.php
@@ -58,24 +58,25 @@
 			}
 		});
 
-	
-		$.ajax({
-			type: 'GET',
-			url:'" . URL::to('/cargarEstados/') ."',
-			dataType:'json',
-			success: function(response) {
-			if (response.success == true) {
-			//console.log(response.estados);
-			$('#estados').html('');
-				$('#estados').append('<option value=\"\">-- Seleccione --</option>');
-				$.each(response.estados,function (k,v){
-				$('#estados').append('<option value=\"'+k+'\">'+v+'</option>');
-			});
-			}else{
-				$('#estados').html('');
-				$('#estados').append('<option value=\"\">-- Seleccione --</option>');
-			}
-			}
+		$('#estados').click(function(){
+				$.ajax({
+					type: 'GET',
+					url:'" . URL::to('/cargarEstados/') ."',
+					dataType:'json',
+					success: function(response) {
+					if (response.success == true) {
+					//console.log(response.estados);
+					$('#estados').html('');
+						$('#estados').append('<option value=\"\">-- Seleccione --</option>');
+						$.each(response.estados,function (k,v){
+						$('#estados').append('<option value=\"'+k+'\">'+v+'</option>');
+					});
+					}else{
+						$('#estados').html('');
+						$('#estados').append('<option value=\"\">-- Seleccione --</option>');
+					}
+					}
+				});
 		});
 	});" 
 }}
