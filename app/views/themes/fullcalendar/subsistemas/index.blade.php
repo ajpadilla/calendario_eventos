1bcb680f36e584cdc9c618c690be2cf4a1600e76
ajpadilla <ajpadilla88@gmail.com>
2014-09-05 20:19:52 -0430

    Agregando ruta para editar y borrar subsistema

--- app/views/themes/fullcalendar/subsistemas/index.blade.php
+++ app/views/themes/fullcalendar/subsistemas/index.blade.php
@@ -12,8 +12,9 @@
 			<tr>
 				<td>{{ $subsistema->id }}</td>
 				<td>{{ $subsistema->nombre }}</td>
-				<td><button  type="button" class="editar btn btn-primary">Editar</button></td>
-				<td><button  type="button" class="borrar btn btn-danger">Borrar</button></td>
+				<td><a href="{{URL::to('editarSubsistema/'.$subsistema->id)}}" class="btn btn-primary">Editar<i class="fa fa-arrow-circle-right"></i></a></td>
+				<td><a href="" class="btn btn-danger">Borrar<i class="fa fa-arrow-circle-right"></i></a></td>
+</tr>
 			</tr>
 			@endforeach
 		</tbody>
