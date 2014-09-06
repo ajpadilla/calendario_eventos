288b976e8b69560a8db568c6bc6ebe091c6af4a8
ajpadilla <ajpadilla88@gmail.com>
2014-09-06 01:26:24 -0430

    Agregando reglas para valida datos del formulario

--- app/views/themes/fullcalendar/persona/partials/validarForm.blade.php
+++ app/views/themes/fullcalendar/persona/partials/validarForm.blade.php
@@ -2,9 +2,12 @@
 {{
 	"
 		$('document').ready(function () {
+			
+			$.validator.setDefaults({
+				debug: true,
+			});
 
 			$('input[name=telefono]').inputmask({'mask':'9999-9999999'});
-			$('input[name=movil]').inputmask({'mask':'9999-9999999'});
 			
 			 $.validator.addMethod('alpha', 
                               function(value, element) {
@@ -46,13 +49,9 @@
         							}
       							}
 							},
-							primer_nombre:{required:!0,alpha: true,rangelength: [1 , 45]},
-							segundo_nombre:{required:!0,alpha: true,rangelength: [1 , 45]},
-							primer_apellido:{required:!0,alpha: true,rangelength: [1 , 45]},
-							segundo_apellido:{required:!0,alpha: true,rangelength: [1 , 45]},
-							fecha_nacimiento:{required:!0,date: true},
+							nombre:{required:!0,alpha: true,rangelength: [1 , 45]},
+							apellido:{required:!0,alpha: true,rangelength: [1 , 45]},
 							sexo:{required:!0},
-							movil:{required:!0},
 							email:{
 									required:!0,
 									email: true,
@@ -102,48 +101,9 @@
 						element.addClass('valid').closest('.form-group').removeClass('has-error').addClass('has-success');
 					}
                  });
-	
-			$('#formWizard').bootstrapWizard({
-				nextSelector:'.nextBtn',
-                previousSelector:'.prevBtn',
-				tabClass: 'nav nav-pills',
-				onNext:function(tab, navigation, index)
-                {
-					
-					var rango = navigation.find('li').length;
-					var next = index + 1;
-					if($('#wizForm').valid() == 1){
-						//alert($('#wizForm').valid())
-						$('.alert-danger').hide();
-                    	$('.stepHeader',$('#formWizard')).text('Step '+(index+1)+' of '+rango);
-						for(var l=navigation.find('li'),d=0;index>d;d++)
-						jQuery(l[d]).addClass('done');
-					}else{
-						return false;
-					}
-					if(next >= rango){
-						$('#formWizard').find('.submitBtn').show();
-					}
-                },
-				onPrevious:function(tab, navigation, index){
-					var rango = navigation.find('li').length;
-                    r=index+1;
-                   	$('.stepHeader',$('#formWizard')).text('Step '+(index+1)+' of '+rango);
-					$('#formWizard').find('.submitBtn').hide();
-				},
-				onTabShow: function(tab, navigation, index){
-					var total = navigation.find('li').length;
-					var current = index+1;
-					var percent = (current/total) * 100;
-					$('#formWizard').find('.progress-bar').css({width:percent+'%'});
-				},
-				onTabClick:function()
-                {
-                	return true;
-                },
-
-			});
-			$('#formWizard').find('.prevBtn').show();
-		 });
+				$('#registrar').click(function(){
+					console.log($('#wizForm').valid());
+				});
+		});
 	"
 }}
