5d3a29d01664e80d267821b66f980b8948a70cb6
ajpadilla <ajpadilla88@gmail.com>
2014-09-07 21:49:12 -0430

    Eliminando wizard

--- app/views/themes/fullcalendar/municipios/formWizard.blade.php
+++ app/views/themes/fullcalendar/municipios/formWizard.blade.php
@@ -1,3 +1,3 @@
 {{ Form::open(array('action' => 'MunicipioController@store','class'=>'form-horizontal','id'=>'formWizardMunicipio','novalidate'=>'novalidate','files'=>true)) }}
-					 @include('themes.fullcalendar.municipios.form')
+	@include('themes.fullcalendar.municipios.form')
 {{ Form::close() }}	
