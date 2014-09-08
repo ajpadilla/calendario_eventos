ae4e0c281e40c4abab57193872398717e275e883
ajpadilla <ajpadilla88@gmail.com>
2014-09-07 21:32:35 -0430

    Agregando metodo index

--- app/controllers/MunicipioController.php
+++ app/controllers/MunicipioController.php
@@ -9,7 +9,8 @@ class MunicipioController extends \BaseController {
 	 */
 	public function index()
 	{
-		//
+		$municipios = Municipio::all();
+        return View::make('themes.fullcalendar.municipios.index')->with('municipios',$municipios);
 	}
 
 
@@ -24,8 +25,8 @@ class MunicipioController extends \BaseController {
 		return View::make('themes.fullcalendar.municipios.create')->with('municipios',$municipios);
 		
 		$municipio = Municipio::find($id);
-          $estado = Estado::find($municipio->estado_id);
-          return View::make('themes.fullcalendar.municipios.editar',compact('municipio','estado'));
+        $estado = Estado::find($municipio->estado_id);
+        return View::make('themes.fullcalendar.municipios.editar',compact('municipio','estado'));
 
 	}
 
