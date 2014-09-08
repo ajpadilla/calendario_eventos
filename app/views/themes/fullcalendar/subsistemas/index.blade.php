2a80e05cd8203afedf5410fcd6bc8ec866a10e9d
ajpadilla <ajpadilla88@gmail.com>
2014-09-07 22:01:25 -0430

    Modificando el blade

--- app/views/themes/fullcalendar/subsistemas/index.blade.php
+++ app/views/themes/fullcalendar/subsistemas/index.blade.php
@@ -1,3 +1,19 @@
+
+@extends('themes.fullcalendar.layouts.form_content')
+@section('form')
+<div class="box border blue">
+<div class="box-title">
+<h4><i class="fa fa-reorder"></i>Estado</h4>
+<div class="tools hidden-xs">
+<a href="#box-config" data-toggle="modal" class="config"><i class="fa fa-cog"></i></a>
+<a href="javascript:;" class="reload"><i class="fa fa-refresh"></i></a>
+<a href="javascript:;" class="collapse"><i class="fa fa-chevron-up"></i></a>
+<a href="javascript:;" class="remove"><i class="fa fa-times"></i></a>
+</div>
+</div>
+<div class="box-body form">
+@if(!$subsistemas->isEmpty())
+<h3>Lista de subsistemas</h3>
 <table class="table table-striped table-bordered">
 	<thead>
 		<tr>
@@ -19,3 +35,13 @@
 			@endforeach
 		</tbody>
 </table>
+@else
+<p>No hay subsistemas!</p>
+@endif
+</div>
+</div>
+@stop
+
+
+
+
