1111cb124af9797daa1219628e03c8a5ea075521
ajpadilla <ajpadilla88@gmail.com>
2014-09-07 22:16:59 -0430

    Modificando vistas y controlador

--- app/views/themes/fullcalendar/municipios/formEditar.blade.php
+++ app/views/themes/fullcalendar/municipios/formEditar.blade.php
@@ -2,8 +2,8 @@
 <div class="form-group">
 		{{ Form::label('estado', 'Estado: ',array('class'=>'control-label col-md-3','id'=>'estado')) }}
 		<div class="col-md-8">
-		{{ Form::select('estados',array($estado->id => $estado->nombre),Input::old('estados'),array('class' => 'form-control','id'=>'estados')) }}
-	</div>
+			{{ Form::select('estados',$estados,$estado,array('class' => 'form-control')) }}
+		</div>
 </div>	
 	<div class="form-group">
 		{{ Form::label('nombre', 'Nombre: ', array('class' => 'col-md-3 control-label')) }}
