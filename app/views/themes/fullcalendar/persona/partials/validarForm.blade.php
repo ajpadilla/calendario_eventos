5a157512e84ce17ffe3f8eea692becbf0857866e
ajpadilla <ajpadilla88@gmail.com>
2014-09-09 00:49:55 -0430

    Agregando nueva validacion

--- app/views/themes/fullcalendar/persona/partials/validarForm.blade.php
+++ app/views/themes/fullcalendar/persona/partials/validarForm.blade.php
@@ -21,7 +21,7 @@
 			errorClass:'error-span',
 			errorElement:'span',
                    	rules:{
-							eventos:{required:true},
+							evento_ids:{required:true},
                     		numero:{required:!0},
                    			telefono:{required:!0},
                         	direccion:{required:!0,rangelength: [10, 256]},
