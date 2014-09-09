d8eeae6aaf6593e5ea4bb0859370d071e2e633c2
ajpadilla <ajpadilla88@gmail.com>
2014-09-09 00:03:54 -0430

    Eliminando peticion para cargar lista de estados

--- app/views/themes/fullcalendar/persona/partials/editarForm.blade.php
+++ app/views/themes/fullcalendar/persona/partials/editarForm.blade.php
@@ -144,24 +144,6 @@
 					}
 				});
 				
-				/*$.ajax({
-					type: 'GET',
-					url:'" . URL::to('/cargarEstados/') ."',
-					dataType:'json',
-					success: function(response) {
-						if (response.success == true) {
-							//console.log(response.estados);
-							$('#estados').html('');
-							$('#estados').append('<option value=\"\">-- Seleccione --</option>');
-							$.each(response.estados,function (k,v){
-								$('#estados').append('<option value=\"'+k+'\">'+v+'</option>');
-							});
-						}else{
-							$('#estados').html('');
-							$('#estados').append('<option value=\"\">-- Seleccione --</option>');
-						}
-					}
-				});*/
 			$('#estados').click(function(){
 				//console.log('algo');
 				//console.log('estado:'+$('#estado').val());
