e597b98cb1f60180cd19cfced5e874613e1da8ab
ajpadilla <ajpadilla88@gmail.com>
2014-09-12 17:30:57 -0430

    Retornando variable booleana en vez de string en el metodo verificar nombre de estado

--- app/controllers/EstadoController.php
+++ app/controllers/EstadoController.php
@@ -75,12 +75,12 @@ class EstadoController extends \BaseController {
 			$nombre_estado= Input::get('nombre');
 			$estado = Estado::where('nombre','=',$nombre_estado)->get();
 			if(count($estado) > 0){
-				return Response::json(array('msg' => 'true'));
+				return Response::json(array('msg' => false));
 			}else{
-				return Response::json(array('msg' => 'false'));
+				return Response::json(array('msg' => true));
 			}
 		}
-		return Response::json(array('respuesta' => 'false'));	 
+		return Response::json(array('respuesta' => false));	 
 	 }
 
 	/**
