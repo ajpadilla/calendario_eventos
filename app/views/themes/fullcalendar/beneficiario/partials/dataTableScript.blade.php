7684af612a2833cfe046bd845ff56790d0e2489f
night zpy <nightzpy@nightzpy.nightzpy.com>
2014-09-13 23:49:36 -0430

    Solucionando errores

--- app/views/themes/fullcalendar/beneficiario/partials/dataTableScript.blade.php
+++ app/views/themes/fullcalendar/beneficiario/partials/dataTableScript.blade.php
@@ -1,17 +1,1 @@
-{{-- Agregando dataTable con los datos de los beneficiarios --}}
-
-{{
-	"
-		$('document').ready(function () {
-			console.log('load');
-			$('#beneficiarios').dataTable({
-				 'processing': true,
-				 'serverSide': true,
-                'ajax': {
-                    'url': '" . URL::to('/retorntarBeneficiarios/') ."',
-                }
-
-			});
-		});
-	"
-}}
+}
