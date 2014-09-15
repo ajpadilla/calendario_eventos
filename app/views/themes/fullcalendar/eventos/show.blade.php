044d3cb9d82d21d72c8bcc67e2d9229fa92f7c2b
night zpy <nightzpy@nightzpy.nightzpy.com>
2014-09-14 21:54:51 -0430

    Editando campo fecha y hora, para solo lectura

--- app/views/themes/fullcalendar/eventos/show.blade.php
+++ app/views/themes/fullcalendar/eventos/show.blade.php
@@ -13,10 +13,14 @@
 </div>
 <div class="box-body form">
 	<h1>Evento {{ $evento->title }}</h1>
-
-	<div class="jumbotron text-center">
-		<h2>{{ $evento->start }}</h2>
-	</div>
+    {{ Form::open(array('action' => 'EventController@store','class'=>'form-horizontal','id'=>'formWizardMunicipio','novalidate'=>'novalidate','files'=>true)) }}
+    <div class="form-group">
+        {{ Form::label('Fecha', 'Fecha - Hora: ', array('class' => 'col-md-3 control-label')) }}
+        <div class="col-md-8">
+            {{ Form::text('nombre',$evento->start, array('class' => 'form-control', 'readonly')) }}
+        </div>
+    </div>
+   {{ Form::close() }}
 </div>
 </div>
 @stop
