d8f893ff037d9f4e9917a58f96c9d2e0581aded7
night zpy <nightzpy@nightzpy.nightzpy.com>
2014-09-15 00:48:51 -0430

    Mostrando datos del evento

--- app/views/themes/fullcalendar/eventos/show.blade.php
+++ app/views/themes/fullcalendar/eventos/show.blade.php
@@ -12,14 +12,78 @@
 </div>
 </div>
 <div class="box-body form">
-	<h1>Evento {{ $evento->title }}</h1>
     {{ Form::open(array('action' => 'EventController@store','class'=>'form-horizontal','id'=>'formWizardMunicipio','novalidate'=>'novalidate','files'=>true)) }}
+        
+     <div class="form-group">
+        {{ Form::label('titulo', 'Titulo: ', array('class' => 'col-md-3 control-label')) }}
+        <div class="col-md-8">
+            {{ Form::text('titulo',$evento->title, array('class' => 'form-control', 'readonly')) }}
+        </div>
+    </div>
+
     <div class="form-group">
-        {{ Form::label('Fecha', 'Fecha - Hora: ', array('class' => 'col-md-3 control-label')) }}
+        {{ Form::label('fecha', 'Fecha - Hora: ', array('class' => 'col-md-3 control-label')) }}
         <div class="col-md-8">
             {{ Form::text('nombre',$evento->start, array('class' => 'form-control', 'readonly')) }}
         </div>
     </div>
+    
+    <div class="form-group">
+        {{ Form::label('estado', 'Estado: ', array('class' => 'col-md-3 control-label')) }}
+        <div class="col-md-8">
+            {{ Form::text('estado',$evento->municipio->estado->nombre, array('class' => 'form-control', 'readonly')) }}
+        </div>
+    </div>
+
+    <div class="form-group">
+        {{ Form::label('municipio', 'Municipio: ', array('class' => 'col-md-3 control-label')) }}
+        <div class="col-md-8">
+            {{ Form::text('municipio',$evento->municipio->nombre, array('class' => 'form-control', 'readonly')) }}
+        </div>
+    </div>
+
+    <div class="form-group">
+        {{ Form::label('articulacion', 'Articulación: ', array('class' => 'col-md-3 control-label')) }}
+        <div class="col-md-8">
+            {{ Form::text('articulacion',$evento->articulacion->nombre, array('class' => 'form-control', 'readonly')) }}
+        </div>
+    </div>
+
+    <div class="form-group">
+        {{ Form::label('subsistema', 'Subsistema: ', array('class' => 'col-md-3 control-label')) }}
+        <div class="col-md-8">
+            {{ Form::text('subsistema',$evento->subsistema->nombre, array('class' => 'form-control', 'readonly')) }}
+        </div>
+    </div>
+
+    <div class="form-group">
+        {{ Form::label('impacto', 'Impacto: ', array('class' => 'col-md-3 control-label')) }}
+        <div class="col-md-8">
+            {{ Form::text('impacto',$evento->impacto->nombre, array('class' => 'form-control', 'readonly')) }}
+        </div>
+    </div>
+
+    <div class="form-group">
+        {{ Form::label('descripcion', 'Descripción: ', array('class' => 'col-md-3 control-label')) }}
+        <div class="col-md-8">
+            {{ Form::textarea('nombre',$evento->descripcion, array('class' => 'form-control','rows'=>'3','cols'=>'3', 'readonly')) }}
+        </div>
+    </div>
+    
+      <div class="form-group">
+        {{ Form::label('direccion', 'Dirección: ', array('class' => 'col-md-3 control-label')) }}
+        <div class="col-md-8">
+            {{ Form::textarea('nombre',$evento->direccion, array('class' => 'form-control','rows'=>'3','cols'=>'3', 'readonly')) }}
+        </div>
+    </div>
+    
+      <div class="form-group">
+        {{ Form::label('observacion', 'Observación: ', array('class' => 'col-md-3 control-label')) }}
+        <div class="col-md-8">
+            {{ Form::textarea('observacion',$evento->observacion, array('class' => 'form-control','rows'=>'3','cols'=>'3', 'readonly')) }}
+        </div>
+    </div>
+
    {{ Form::close() }}
 </div>
 </div>
