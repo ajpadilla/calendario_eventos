ec2b0c6d300782efcfaec450ed1975e222735c42
night zpy <nightzpy@nightzpy.nightzpy.com>
2014-09-23 11:03:19 -0430

    Agregando usuarios de prueba a la bd

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder {
 		 $this->call('EventosTableSeeder');
 		 $this->call('PersonasTableSeeder');
 		 $this->call('BeneficiariosTableSeeder');
-        
+         $this->call('UsersTableSeeder');
 	}
 
 }
