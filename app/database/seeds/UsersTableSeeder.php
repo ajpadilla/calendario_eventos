c040c05e243e1003f7b449fc1a9303dd906962d6
night zpy <nightzpy@nightzpy.nightzpy.com>
2014-09-23 11:02:41 -0430

    Creando datos de prueba para los usuarios del sistema

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -6,7 +6,17 @@ class UsersTableSeeder extends Seeder {
 
 	public function run()
 	{
-		
+		$date = new DateTime;
+		$usuarios [] = array(
+            'username' => 'christopher.pitt',
+            'password'=> Hash::make('7h3¡MOST!53cu23'),
+            'email'    => 'chris@example.com',
+            'created_at' => $date->format('Y-m-d h:m:s'),
+            'updated_at' => $date->format('Y-m-d h:m:s')            
+        );  
+
+		// Uncomment the below to run the seeder
+        DB::table('users')->insert($usuarios);
 	}
 
 }
