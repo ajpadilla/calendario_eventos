87ddbcf1a6f5a41f846e52f7b308cfca56d401df
night zpy <nightzpy@nightzpy.nightzpy.com>
2014-09-26 15:12:02 -0430

    Controlador para el inicio de sesion de los usuarios

---
+++ app/controllers/UserController.php
@@ -0,0 +1,124 @@
+<?php
+
+class UserController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		if ($this->isPostRequest()) 
+		{
+			$validator = $this->getLoginValidator();
+
+			if ($validator->passes()) {
+				$credentials = $this->getLoginCredentials();
+				if (Auth::attempt($credentials)) {
+					return View::make('themes.fullcalendar.eventos.create');
+				}
+
+				return Redirect::back()->withErrors([
+					"password" => ["Password Invalido."]
+					]);
+			} else {
+				return Redirect::back()
+				->withInput()
+				->withErrors($validator);
+			}
+		}
+		return View::make('themes.fullcalendar.layouts.sections');
+  }
+  
+  protected function isPostRequest()
+  {
+    return Input::server("REQUEST_METHOD") == "POST";
+  }
+  
+  protected function getLoginValidator()
+  {
+    return Validator::make(Input::all(), [
+      "username" => "required",
+      "password" => "required"
+    ]);
+  }	
+
+  protected function getLoginCredentials()
+  {
+    return [
+      "username" => Input::get("username"),
+      "password" => Input::get("password")
+    ];
+  }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+
+}
