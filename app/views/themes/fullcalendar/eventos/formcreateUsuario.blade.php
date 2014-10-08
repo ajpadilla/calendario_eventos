9bea4fcef863a67997b99064e752817b9a6386c5
Alvaro Padilla <ajpadilla88@gmail.com>
2014-10-08 17:52:31 -0430

    Agregando nueva vista

---
+++ app/views/themes/fullcalendar/eventos/formcreateUsuario.blade.php
@@ -0,0 +1,40 @@
+@extends('themes.fullcalendar.calendario.form')
+
+@section('script')
+	@include('themes.fullcalendar.eventos.partials.validarDatos')
+@stop()
+
+ 
+@section('page_header')
+      <!-- /BREADCRUMBS -->
+      <div class="clearfix">
+          <h3 class="content-title pull-left"></h3>
+      </div>
+      <div class="description"></div>
+@stop
+  
+@section('form')
+	<div class="box border blue">
+          <div class="box-title">
+              <h4><i class="fa fa-reorder"></i>Agregar Evento</h4>
+              <div class="tools hidden-xs">
+                  <a href="#box-config" data-toggle="modal" class="config">
+                      <i class="fa fa-cog"></i>
+                  </a>
+                  <a href="javascript:;" class="reload">
+                      <i class="fa fa-refresh"></i>
+                  </a>
+                  <a href="javascript:;" class="collapse">
+                      <i class="fa fa-chevron-up"></i>
+                  </a>
+                  <a href="javascript:;" class="remove">
+                      <i class="fa fa-times"></i>
+                  </a>
+              </div>
+          </div>
+          <div class="box-body form">
+				@include('themes.fullcalendar.eventos.formWizard')
+          </div>
+	</div>
+@stop
+
