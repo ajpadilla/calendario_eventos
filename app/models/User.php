58763958ec1730e3f5c934be62660619b02f196a
Alvaro Padilla <ajpadilla88@gmail.com>
2014-10-08 17:09:34 -0430

    Usando el plugin estrust, para determinar el rol de usuario

--- app/models/User.php
+++ app/models/User.php
@@ -4,9 +4,10 @@ use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
+use Zizaco\Entrust\HasRole;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
-
+	use HasRole;
 	use UserTrait, RemindableTrait;
 
 	/**
