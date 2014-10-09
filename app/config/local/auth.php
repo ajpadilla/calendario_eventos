a5ac124bbcaa944a600a0b7fd30649643515a96f
Alvaro Padilla <ajpadilla88@gmail.com>
2014-10-09 12:24:14 -0430

    Actualizando datos

--- app/config/local/auth.php
+++ app/config/local/auth.php
@@ -41,7 +41,7 @@ return array(
 	|
 	*/
 
-	'table' => 'userss',
+	'table' => 'users',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -60,9 +60,9 @@ return array(
 
 	'reminder' => array(
 
-		'email' => 'emails.auth.reminder',
+		"email"  => "emails.auth.reminder",
 
-		'table' => 'password_reminders',
+		"table"  => "token",
 
 		'expire' => 60,
 
