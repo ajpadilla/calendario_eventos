9ffe732555c0d030ebe6cfdebe24688722ca05dd
Alvaro Padilla <ajpadilla88@gmail.com>
2014-10-15 22:17:25 -0430

    Borrando boton borrar del index

--- app/views/themes/fullcalendar/impactos/index.blade.php
+++ app/views/themes/fullcalendar/impactos/index.blade.php
@@ -19,7 +19,6 @@
 					<td>Id</td>
 					<td>Nombre</td>
 					<td>Editar</td>
-					<td>Borrar</td>
 				</tr>
 			</thead>
 				<tbody>
@@ -28,7 +27,6 @@
 						<td>{{ $impacto->id }}</td>
 						<td>{{ $impacto->nombre }}</td>
 						<td><a href="{{URL::to('editarImpacto/'.$impacto->id)}}" class="btn btn-primary">Editar<i class="fa fa-arrow-circle-right"></i></a></td>
-		<td><a href="" class="btn btn-danger">Borrar<i class="fa fa-arrow-circle-right"></i></a></td>
 					</tr>
 					@endforeach
 				</tbody>
